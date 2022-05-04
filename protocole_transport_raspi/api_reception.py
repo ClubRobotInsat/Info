ff1fa62468520571bffc57ddc9da135c64d70339
Aude510 <dunae97300@outlook.fr>
2022-05-04 14:45:39 +0200

    traitement message et mise dans buffer

--- protocole_transport_raspi/api_reception.py
+++ protocole_transport_raspi/api_reception.py
@@ -3,20 +3,86 @@ import subprocess
 from time import sleep
 from threading import Thread
 
-id_raspi = 0 
 
-def process_message_reçu(mess):
+id_raspi = 0 
+# nb de stm/raspi connectés
+nb_disp=16
+# nb de messages différents pouvant être envoyés en simultané par le même dispositif
+nb_mess=8
+# nb de trames max par message 
+nb_trames = 16 
+keys = [(id_or,id_mes) for id_or in range(nb_disp) for id_mes in range(nb_mess)]
+# buffer pour l'application : dictionnaire avec key = (id_or,id_mes) 
+# liste contenant les divers datas 
+buffer = {key:[None]*nb_trames for key in keys}
+
+def test_variables(id_dest,id_or,id_mes,seq,ack): 
+    print("variables récupérées :")
+    print("id_dest =",id_dest)
+    print("id_or =",id_or)
+    print("id_mes =",id_mes)
+    print("seq =",seq)
+    print("ack =",ack)
+
+
+def test_buffer(id_or,id_mes):
+    print("message placé dans le buffer : ")
+    for txt in reversed(buffer[(id_or,id_mes)]):
+        if txt != None : 
+            print(txt)
+
+def process_mess(mess):
     print("process message...")
-    #récupérer id dans une variable 
+    #récupérer les différents champs de l'en-tête dans des variables
+    mess=mess.split(" ") ## TODO adapter au format des messages reçus par le candump 
+    id_dest = int(mess[0])>>4
+    #si le message est pas pour moi return 
+    if id_dest != id_raspi:
+        print("ce message n'est pas pour moi")
+        return 
     #si le message est pour moi, traiter et mettre dans buffer 
-    #sinon break 
-    print(mess)
+    id_or = int(mess[0])&15 
+    id_mes = int(mess[1])>>5
+    seq = (int(mess[1])>>1)&15
+    ack = int(mess[1])&1   
+    # test_variables(id_dest,id_or,id_mes,seq,ack)
+    buffer[(id_or,id_mes)][seq] = mess[2:]
+    print("message processé!")
+    return (id_or,id_mes)
+
+def test_reception(): 
+    print("--------------------------------test de la réception------------------------------------")
+
+    # 0000 0001 001 0010 0
+    print("appel de process mess avec en tête 0 1 1 2 0")
+    process_mess("1 36 hola tu")
+    print("\n")
+
+    # 0100 0001 001 0010 0 
+    print("appel de process mess avec en tête 4 1 1 2 0")
+    process_mess("65 36 hola tu")
+    print("\n")
+
+    print("test de la mise dans le buffer")
+    print("data envoyée : hola tu que tal kfjdjk")
+    print("\n")
+
+    # 0000 0001 001 0001 0 
+    process_mess("1 34 que tal")
+    print("\n")
+    # 0000 0001 001 0000 0 
+    (id_or,id_mes) = process_mess("1 32 kfjdjk")
+    print("\n")
+    test_buffer(id_or,id_mes)
+    print("--------------------------------------------------------------------------------------------")
 
 # réception 
-#reception = subprocess.Popen(["candump","any"],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-reception=subprocess.Popen(['ping', 'geekflare.com'], stdout=subprocess.PIPE, text=True)
-while True: 
-    output=reception.stdout.readline()
-    if output:
-        process_message_reçu(output.strip()) #output.strip() est un string 
+def reception():
+    reception = subprocess.Popen(["candump","any"],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    # reception=subprocess.Popen(['ping', 'geekflare.com'], stdout=subprocess.PIPE, text=True)
+    while True: 
+        output=reception.stdout.readline()
+        if output:
+            process_mess(output.strip()) #output.strip() est un string 
 
+test_reception()
