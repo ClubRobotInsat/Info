c0c13b5188da3745defbbc329c675253f93baa1e
Aude510 <dunae97300@outlook.fr>
2022-05-06 20:42:48 +0200

    queue, les tests marchent mais pas le vrai code

---
+++ protocole_transport_raspi/test_queue.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process, Queue
+from time import sleep 
+from test_queue2 import f1
+from test_queue3 import f2
+
+if __name__ == '__main__':
+    q = Queue()
+    p = Process(target=f1, args=(q,))
+    p1 = Process(target=f2,args=(q,))
+    p1.start()
+    p.start()
+    print("j'attends...")
+    # print(q.get())    # prints "[42, None, 'hello']"
+    sleep(7)
+    print(q.get()) 
+    p.join()
+    p1.join()
---
+++ protocole_transport_raspi/test_queue2.py
@@ -0,0 +1,5 @@
+from time import sleep
+
+def f1(q):
+    q.put([42, None, 'hello'])
+
