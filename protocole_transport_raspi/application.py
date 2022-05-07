a542bf98cec6b340a9477d4cea333cb8543fd6e3
Aude510 <dunae97300@outlook.fr>
2022-05-07 18:07:10 +0200

    queue fonctionnelle

--- protocole_transport_raspi/application.py
+++ protocole_transport_raspi/application.py
@@ -1,13 +1,7 @@
 from api_envoi import main as envoyer
-from utiles import Message
-from time import sleep
 
 def main(q):
-    print("\n-----------------------------hello world--------------------------\n")
-    print("J'attends...")
-    sleep(1)
-    print("j'ai fini d'attendre")
     # q.put("hello world")
+    print("queue est vide :",q.empty())
     message=q.get() # le get est bloqué en attente visiblement, la queue est vide 
-    print("j'ai bien pris la data")
     print("data reçue dans app : ",message.data)
--- protocole_transport_raspi/test_queue2.py
+++ protocole_transport_raspi/test_queue2.py
@@ -1,5 +1,6 @@
 from time import sleep
 
 def f1(q):
+    print("f1 met dans la queue")
     q.put([42, None, 'hello'])
 
