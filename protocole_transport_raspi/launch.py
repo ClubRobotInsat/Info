fa377b6d4adb13a03f94fe26c0e2f6d2ebbbe638
Aude510 <dunae97300@outlook.fr>
2022-05-19 14:39:45 +0200

    commit de sauvegarde avant de tout changer en multithreading

--- protocole_transport_raspi/launch.py
+++ protocole_transport_raspi/launch.py
@@ -1,17 +1,18 @@
 from api_reception import test_reception as reception
 from application import main as app
-from multiprocessing import Process, Queue, Condition, Lock
+from multiprocessing import Process, Queue, Condition, Lock, Manager
 from api_envoi import test_envoi
-from utiles import buffer_acks
+from utiles import keys
 from time import sleep
 
 if __name__ == '__main__':  # ne s'exécute que si le fichier est lancé directement (pas comme librairie)
     q = Queue()
-    ack_received = Condition()
-    lock_buffer_envoi = Lock()
-    proc_app = Process(target=app, args=(q, ack_received, lock_buffer_envoi, buffer_acks))
-    proc_test_envoi = Process(target=test_envoi, args=(ack_received, lock_buffer_envoi))
-    proc_reception = Process(target=reception, args=(q, ack_received, lock_buffer_envoi, buffer_acks))
+    manager = Manager()
+    buffer_acks = manager.dict()
+    buffer_acks = {key: 0 for key in keys}
+    proc_app = Process(target=app, args=(q, buffer_acks, ))
+    proc_test_envoi = Process(target=test_envoi, args=(buffer_acks, ))
+    proc_reception = Process(target=reception, args=(q, buffer_acks, ))
     proc_test_envoi.start()
     proc_reception.start()
     proc_app.start()
