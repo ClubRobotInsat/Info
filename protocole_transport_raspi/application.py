4a58d5433dd8721641b0badda8f54a69e30400ec
Aude510 <dunae97300@outlook.fr>
2022-05-19 18:15:14 +0200

    en fait je fais de la merde depuis le début donc j'essaie de m'envoyer mes propres acks, ça ira mieux

--- protocole_transport_raspi/application.py
+++ protocole_transport_raspi/application.py
@@ -1,6 +1,7 @@
 import utiles
 from api_envoi import envoyer
-from utiles import Message
+from utiles import Message, Trame
+import subprocess
 from utiles import id_raspi
 
 
@@ -17,6 +18,19 @@ def main(q, buffer_acks, ack_received_cond, buffer_lock):
     envoyer(Message(3, 0,
                     ['FF', 'EE', 'AA', 'AA', 'CC', 'BB', '01', '01', '01', '01', '01', '01', '01', '01', '01',
                      '01', '01', '01']), buffer_acks, ack_received_cond, buffer_lock)
+
+    # je m'envoie mes propres acks, oui c'est moche
+
+    subprocess.Popen(["cansend", "can0", "000#03 03 00 00 00 00 00 00"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+
+
+    subprocess.Popen(["cansend", "can0", "000#03 01 00 00 00 00 00 00"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+
+
+    subprocess.Popen(["cansend", "can0", "000#03 05 00 00 00 00 00 00"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
 
     while True:
         if not q.empty():
