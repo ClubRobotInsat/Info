34048429f93bc4bf21b93085933cf59ed30cc4ee
Aude510 <dunae97300@outlook.fr>
2022-11-10 18:05:33 +0100

    erreurs minimes

--- 2022-2023/test_envoi.py
+++ 2022-2023/test_envoi.py
@@ -1,8 +1,9 @@
 from utiles import tab_ids
 from envoi import envoyer
-
+from time import sleep
 
 for prio in range(1,8):
     for dest in tab_ids.keys():
-        envoyer(prio,dest, "hola")
+        sleep(1) #TODO : traiter l'exception quand on essait d'envoyer trop de trucs en même temps
+        envoyer(prio,dest,"hola")
 
