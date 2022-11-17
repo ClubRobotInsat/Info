1b74dc7a0c3b9d9737dcfffedab8681e97b8a13e
Aude510 <dunae97300@outlook.fr>
2022-11-17 17:56:27 +0100

    des commentaires

--- 2022-2023/envoi.py
+++ 2022-2023/envoi.py
@@ -15,8 +15,8 @@ bus = can.Bus(interface='socketcan',
 def construire_en_tete(prio, id_dest, id_or):
     return (prio << 8) + (id_dest << 4) + id_or
 
-# prio int, dest string, srt_data string
-def envoyer(prio, dest, str_data): # TODO : tableau avec les actions à effectuer et une correspondance en bytes direct
+# prio int, dest string, data string (pour l'instant) 
+def envoyer(prio, dest, data): # TODO : gérer correspondance actions / bytes
     id_dest=tab_ids[dest]
     id_or=tab_ids["raspi"]
     en_tete=construire_en_tete(prio, id_dest, id_or)
@@ -24,7 +24,7 @@ def envoyer(prio, dest, str_data): # TODO : tableau avec les actions à effectue
     # arbitration_id est un int!!
     # is_extended_id=False car id de 11 bits
     # data est un tableau d'octets!!
-    message=can.Message(check=True, arbitration_id=en_tete, is_extended_id=False, data=bytearray(str_data,encoding="utf-8"))
+    message=can.Message(check=True, arbitration_id=en_tete, is_extended_id=False, data=bytearray(data, encoding="utf-8"))
     bus.send(message, timeout=0.2)
     # en l'état valeur par défaut pour timeout
     # TODO : est-ce que ce timeout doit être changé?
--- 2022-2023/utiles.py
+++ 2022-2023/utiles.py
@@ -4,3 +4,4 @@ tab_ids={"raspi":1, "herkulex":2, "base roulante":3}
 
 reversed_tab_ids=dict((v,k) for (k,v) in tab_ids.items())
 
+# TODO : chaque objet doit avoir une méthode to_bytes() pour passer les commandes / arguments à bytes 
